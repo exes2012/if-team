9dccab1139d1e56cbf288a07f3e3f78315b401b0
Valeriy Nashchekin <exes2012@gmail.com>
2022-06-21 09:53:02 +0300

    Finishing header, adding button, icon component

--- src/default-board.js
+++ src/default-board.js
@@ -12,6 +12,7 @@ export default {
                     id: uuid(),
                     userAssigned:null
                 }
+
             ]
         }
     ]
--- tailwind.config.js
+++ tailwind.config.js
@@ -98,21 +98,12 @@ module.exports = {
     },
 
     screens: {
-      'sm': '640px',
-      // => @media (min-width: 640px) { ... }
-
-      'md': '768px',
-      // => @media (min-width: 768px) { ... }
-
-      'lg': '1024px',
-      // => @media (min-width: 1024px) { ... }
-
-      'xl': '1280px',
-      // => @media (min-width: 1280px) { ... }
-
-      '2xl': '1536px',
-      // => @media (min-width: 1536px) { ... }
-      '3xl': '1840px',
+      "3xl": { max: "1840px" },
+      "2xl": { max: "1535px" },
+      xl: { max: "1279px" },
+      lg: { max: "1023px" },
+      md: { max: "767px" },
+      sm: { max: "639px" },
     },
 
     fontFamily: {
