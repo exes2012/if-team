00df800fb194b1b555ce7fac9aaa38fc0637af47
Andy <53950662+AdrianRazor@users.noreply.github.com>
2022-07-04 11:15:06 +0300

    created Dropzone component

---
+++ src/components/VDropzone.vue
@@ -0,0 +1,69 @@
+<template>
+  <div
+    @dragenter.prevent="toggleActive"
+    @dragleave.prevent="toggleActive"
+    @dragover.prevent
+    @drop.prevent="toggleActive"
+    :class="{ active: active }"
+    class="dropzone"
+  >
+    <input type="file" id="dropzone" class="hidden dropzone-file" />
+    <label for="dropzone" class="text-3">Drag the file here</label>
+  </div>
+</template>
+
+<script>
+import { ref } from "vue";
+export default {
+  name: "VDropzone",
+  setup() {
+    const active = ref(false);
+
+    const toggleActive = () => {
+      active.value = !active.value;
+    };
+
+    return { active, toggleActive };
+  },
+};
+</script>
+
+<style scoped>
+.dropzone {
+  @apply flex
+    justify-center
+    items-center
+    cursor-pointer
+    w-full
+    h-14
+    rounded-md
+    border
+    border-dashed
+    border-gray-400
+    transition
+    hover:border-blue-400
+    hover:bg-blue-100
+    hover:text-blue-400;
+}
+.dropzone label {
+  @apply w-full
+  h-full
+  flex
+  justify-center
+  items-center
+  cursor-pointer
+  text-gray-400
+  hover:text-blue-400
+  transition;
+}
+
+.dropzone.active {
+  @apply border-blue-400
+    bg-blue-100
+    text-blue-400;
+}
+.dropzone.active label {
+  @apply text-blue-400
+  transition;
+}
+</style>
--- src/views/ViewPayments.vue
+++ src/views/ViewPayments.vue
@@ -1,18 +1,60 @@
 <template>
-	<div class="bg-gray-100 w-full h-screen">
-		<v-container>
-			<v-card class="w-full h-96 mt-32">
-			</v-card>
-		</v-container>
-	</div>
+  <div class="bg-gray-100 w-full h-screen">
+    <v-container class="flex-col">
+      <v-card class="w-full h-96 mt-32 mb-10 p-8">
+        <ul class="flex items-center flex-wrap gap-4 mb-4">
+          <li
+            class="w-fit flex items-center py-[5px] px-2.5 rounded-md bg-gray-100"
+          >
+            <div class="w-[26px] h-[26px] rounded-md overflow-hidden mr-2.5">
+              <img
+                src="../assets/img/file-upload-demo.jpg"
+                alt="file icon"
+                class="w-full h-full object-cover"
+              />
+            </div>
+            <p class="text-4 mr-2.5">Unnamed.png (2.33 Mb)</p>
+            <v-button type="button"
+              ><v-icon
+                name="close"
+                width="17"
+                height="17"
+                viewBox="0 0 20 20"
+                class="hover:fill-blue-400"
+              ></v-icon>
+            </v-button>
+          </li>
+        </ul>
+        <v-dropzone @drop.prevent="drop" @change="selectedFile"></v-dropzone>
+        <span>File: {{ dropzoneFile.name }}</span>
+      </v-card>
+    </v-container>
+  </div>
 </template>
 
 <script>
+import VDropzone from "../components/VDropzone.vue";
+import { ref } from "vue";
+
 export default {
-	name: "ViewPayments"
-}
+  name: "ViewPayments",
+  components: {
+    VDropzone,
+  },
+  setup() {
+    let dropzoneFile = ref("");
+
+    const drop = (e) => {
+      dropzoneFile.value = e.dataTransfer.files[0];
+    };
+
+    const selectedFile = () => {
+      dropzoneFile.value = document.querySelector(".dropzone-file").files[0];
+    };
+
+    return { dropzoneFile, drop, selectedFile };
+  },
+};
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
