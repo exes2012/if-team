ca2b7d241502737953595fd07891f32e348b3bd5
Andy <53950662+AdrianRazor@users.noreply.github.com>
2022-07-06 10:03:36 +0300

    created LinkHeaderNavigation component

---
+++ src/components/LinkHeaderNavigation.vue
@@ -0,0 +1,23 @@
+<template>
+  <li>
+    <router-link :to="data.url" class="flex items-center">
+      <v-icon :name="data.icon" />
+      <div class="text-sm ml-3">{{ data.name }}</div>
+    </router-link>
+  </li>
+</template>
+
+<script>
+export default {
+  name: "LinkHeaderNavigation",
+  components: {},
+
+  props: {
+    data: {
+      type: Object,
+    },
+  },
+};
+</script>
+
+<style scoped></style>
--- src/components/TheHeaderNavigation.vue
+++ src/components/TheHeaderNavigation.vue
@@ -1,27 +1,34 @@
 <template>
-	<ul class="flex">
-		<li v-for="route in routes" :key="route.id" class="mr-10 flex">
-			<router-link :to="route.url" class="flex items-center">
-				<v-icon :name="route.icon"/>
-				<div class="text-sm ml-3">{{ route.name }}</div>
-			</router-link>
-		</li>
-	</ul>
+  <ul class="flex">
+    <link-header-navigation
+      v-for="route in routes"
+      :key="route.id"
+      :data="route"
+      class="mr-10 flex"
+    >
+    </link-header-navigation>
+  </ul>
 </template>
 
 <script>
-export default {
-	name: "TheHeaderNavigation",
-	components: {},
+import LinkHeaderNavigation from "./LinkHeaderNavigation.vue";
 
-	props:{
-		routes:{
-			type:Array
-		}
-	}
-}
+export default {
+  name: "TheHeaderNavigation",
+  components: {
+    LinkHeaderNavigation,
+  },
+
+  props: {
+    routes: {
+      type: Array,
+    },
+  },
+
+  created() {
+    console.log("NAV", this.routes);
+  },
+};
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
